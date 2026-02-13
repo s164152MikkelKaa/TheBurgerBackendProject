7242151ec83dffdbc2eb79f3fef53d5c0037db9a
s164152MikkelKaa <mikkel.kaa@gmail.com>
2026-02-13 10:56:03 +0100

    Initial setup of DTO to login on the backend.

---
+++ TheBurgerBackendProject/DTOs/UserAccountDTOs.cs
@@ -0,0 +1,17 @@
+﻿namespace TheBurgerBackendProject.DTOs
+{
+    public class UserAccountDTOs
+    {
+    }
+
+    public class UserLoginDTO
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class UserNewDTO : UserLoginDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+    }
+}
