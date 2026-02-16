3f77e451b6fdba178c1e6b8e063e68979fe9fb42
s164152MikkelKaa <mikkel.kaa@gmail.com>
2026-02-16 14:28:21 +0100

    Backend user login, logout and register functionality, still missing API endpoints tho.

--- TheBurgerBackendProject/Services/AccountStateService.cs
+++ TheBurgerBackendProject/Services/AccountStateService.cs
@@ -1,11 +1,72 @@
-﻿namespace TheBurgerBackendProject.Services
+﻿using Microsoft.AspNetCore.Identity;
+using TheBurgerBackendProject.DTOs;
+
+namespace TheBurgerBackendProject.Services
 {
     public interface IAccountStateService
     {
-
+        Task<(bool Success, string? ErrorMessage)> LoginUserAsync(UserLoginDTO loginInfo);
+        Task<(bool Success, string? ErrorMessage)> LogoutUserAsync();
+        Task<(bool Success, string? ErrorMessage)> RegisterUserAsync(UserNewDTO user);
     }
 
     public class AccountStateService : IAccountStateService
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountStateService(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public async Task<(bool Success, string? ErrorMessage)> RegisterUserAsync(UserNewDTO user)
+        {
+            var registerUser = new IdentityUser { Email = user.Email, EmailConfirmed = true, UserName = user.UserName };
+            var result = await _userManager.CreateAsync(registerUser, user.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(registerUser, isPersistent: false);
+                return (true, null);
+            }
+            string theErrorDescription = "";
+            foreach (var theError in result.Errors)
+            {
+                theErrorDescription += theError.Description + ", ";
+            }
+            return (false, theErrorDescription);
+        }
+
+        public async Task<(bool Success, string? ErrorMessage)> LoginUserAsync(UserLoginDTO loginInfo)
+        {
+            var (success, errorMessage, theUser) = await GetUserByEmailAsync(loginInfo.Email);
+            if (!success)
+            {
+                return (success, errorMessage);
+            }
+            var result = await _signInManager.PasswordSignInAsync(theUser, loginInfo.Password, isPersistent: false, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return (true, null);
+            }
+            return (false, "Can't sign in.");
+        }
+
+        private async Task<(bool Success, string? ErrorMessage, IdentityUser? theUser)> GetUserByEmailAsync(string email)
+        {
+            var result = await _userManager.FindByEmailAsync(email);
+            if (result == null)
+            {
+                return (false, "No matching e-mail", null);
+            }
+            return (true, null, result);
+        }
+
+        public async Task<(bool Success, string? ErrorMessage)> LogoutUserAsync()
+        {
+            await _signInManager.SignOutAsync();
+            return (true, null);
+        }
     }
 }
